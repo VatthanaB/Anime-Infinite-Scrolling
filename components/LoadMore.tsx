691c4e00e757f3ef86e58a23ce3d9111fcae3174
Vatthana Boulom <boulom.vatthana.pro@gmail.com>
2024-01-12 19:06:51 +1300

    Update LoadMore component and package dependencies

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -25,13 +25,7 @@ function LoadMore() {
       </section>
       <section className="flex justify-center items-center w-full">
         <div ref={ref}>
-          <Image
-            src="./spinner.svg"
-            alt="spinner"
-            width={56}
-            height={56}
-            className="object-contain"
-          />
+          <span className="loading loading-spinner text-error w-12"></span>
         </div>
       </section>
     </>
